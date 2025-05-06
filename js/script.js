d78f98f0f06f680447f4cdac00d8a34121cc9ae6
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-06 17:27:00 +0000

    Damn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha Vorontsov
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+
+function calculaterandomnumber () {
+  Let
+  const Results1 = parseInt(document.getElementById('firstvalue').value)
+  const Results2 = parseInt(document.getElementById('secondvalue').value)
+
+
+  }
+
+}
